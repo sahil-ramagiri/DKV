16cbcd7cd8b141d57b7b62ac948565ba1e215e99
Sahil Kumar Ramagiri <ramagirisahil1@gmail.com>
2024-02-29 20:29:25 -0500

    Intial Project Structure

---
+++ dkv-client/src/test/java/sk/dkvclient/DkvClientApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.dkvclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkvClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ dkv-node/src/main/java/sk/dkv/DkvApplication.java
@@ -0,0 +1,13 @@
+package sk.dkv;
+
+public class DkvApplication {
+
+	public static void main(String[] args) {
+		init();
+	}
+
+	public static void init() {
+		System.out.println("Started Application");
+	}
+
+}
